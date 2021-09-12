5a2db413d265d2b06e3c47fce810ba603fc8afa8
Ganesh <ganeshgo1999@gmail.com>
2021-09-12 18:29:36 +0530

    Eureka, Circuit Breaker, Config Server, Api Gateway

--- catalog-service/TableCreation.sql
+++ catalog-service/TableCreation.sql
@@ -1,19 +1,8 @@
 CREATE DATABASE catalog;
-use catalog;
-DROP TABLE products;
+USE catalog;
+
 CREATE TABLE products (id BIGINT AUTO_INCREMENT, code VARCHAR(10), name VARCHAR(20), description VARCHAR(30), price DOUBLE, PRIMARY KEY (id));
 
-
-INSERT INTO products VALUES
-(NULL,'P1001','Snow Shovel','Basic 22 inch',9.99),
-(NULL,'P1002','Snow Shovel','Deluxe 24 inch',19.99),
-(NULL,'P1003','Snow Shovel','Super Deluxe 26 inch',49.99),
-(NULL,'P1004','Ice Scraper','Windshield 4 inch',3.99),
-(NULL,'P1005','Roof Rake','Lightweight Metal Rake 65 Inch',27.99),
-(NULL,'P1006','Roof Rake','Lightweight Metal Rake 35 Inch',17.99);
-
-
-TRUNCATE TABLE products;
 
 INSERT INTO products
 VALUES (NULL, 'P1001', 'Snow Shovel', 'Basic 22 inch', 9.99),
